74a5b2da042b3731307a254bc7d907e846c02029
Farizpurnomo <farizpurnomo91@gmail.com>
2020-04-14 10:21:56 +0700

    android
    slide home sudah benar,  bottom navigation sudah ada. yang tinggal ganti gambarnya semua, API belum sama sekali

---
+++ file fix/HorizontalList/app/src/main/java/com/example/msi/horizontallist/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.msi.horizontallist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView mList1,mList2;
+    List<App> appList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mList1 = findViewById(R.id.list1);
+        appList = new ArrayList<>();
+
+        appList.add(new App(R.drawable.nambah_produk_home_ps,"youtube",80));
+        appList.add(new App(R.drawable.nambah_produk_home_ps,"haha",40));
+        appList.add(new App(R.drawable.nambah_produk_home_ps,"hihi",40));
+
+        LinearLayoutManager manager1 = new LinearLayoutManager(this);
+        manager1.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mList1.setLayoutManager(manager1);
+
+        LinearLayoutManager manager2 = new LinearLayoutManager(this);
+        manager2.setOrientation(LinearLayoutManager.HORIZONTAL);
+        mList1.setLayoutManager(manager2);
+
+        CustomAdaptor adaptor1 = new CustomAdaptor(this,appList);
+        mList1.setAdapter(adaptor1);
+
+    }
+}
