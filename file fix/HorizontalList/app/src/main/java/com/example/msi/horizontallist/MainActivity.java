ada0cc2b058085534df4b5fe1d037e0775f1ef74
Farizpurnomo <farizpurnomo91@gmail.com>
2020-04-15 19:28:02 +0700

    update adobe xd dan penambahan toolbar di android
    android dan adobe xd

--- file fix/HorizontalList/app/src/main/java/com/example/msi/horizontallist/MainActivity.java
+++ file fix/HorizontalList/app/src/main/java/com/example/msi/horizontallist/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.msi.horizontallist;
 
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,12 +22,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        getSupportActionBar().hide();
+
+
         mList1 = findViewById(R.id.list1);
         appList = new ArrayList<>();
 
-        appList.add(new App(R.drawable.nambah_produk_home_ps,"youtube",80));
+        appList.add(new App(R.drawable.halal_info_news,"youtube",80));
         appList.add(new App(R.drawable.nambah_produk_home_ps,"haha",40));
-        appList.add(new App(R.drawable.nambah_produk_home_ps,"hihi",40));
+        appList.add(new App(R.drawable.slide_berita_ps,"hihi",40));
 
         LinearLayoutManager manager1 = new LinearLayoutManager(this);
         manager1.setOrientation(LinearLayoutManager.HORIZONTAL);
